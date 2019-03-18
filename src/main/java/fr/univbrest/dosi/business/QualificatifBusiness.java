7759f8acbfc9fa01cbbca74b9e37b5ae6ebcfd98
KARIM <abdelkarim.chaara@gmail.com>
2019-03-18 15:18:06 +0100

    Berrou : Qualification Added

---
+++ src/main/java/fr/univbrest/dosi/business/QualificatifBusiness.java
@@ -0,0 +1,36 @@
+package fr.univbrest.dosi.business;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import fr.univbrest.dosi.bean.Qualificatif;
+
+public interface QualificatifBusiness {
+	
+
+	
+
+	public List<Qualificatif> GetAllQualificatifs();
+
+
+	void deleteQualificatif(Integer idQualificatif);
+
+
+	public Qualificatif createQualificatif(Qualificatif qualificatif);
+	
+	public Qualificatif findQualificationById(Integer idQualificatif);
+	
+	public List<Qualificatif> findQualificationByMin(String minimal);
+	
+	public List<Qualificatif> findQualificationByMax(String maximal);
+	
+	public boolean findIfIdQualificatifExistsInReponse(Integer idQualificatif);
+	
+	public Qualificatif updateQualificatifById(Qualificatif qualificatif);
+	
+	
+	
+	
+	
+
+}
