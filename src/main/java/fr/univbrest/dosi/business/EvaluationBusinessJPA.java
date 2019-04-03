81a09231700555b8a85f622c6c86e8a043f5f5e6
KARIM <abdelkarim.chaara@gmail.com>
2019-04-03 17:45:18 +0200

    evaluation POST fixed

--- src/main/java/fr/univbrest/dosi/business/EvaluationBusinessJPA.java
+++ src/main/java/fr/univbrest/dosi/business/EvaluationBusinessJPA.java
@@ -1,9 +1,16 @@
 package fr.univbrest.dosi.business;
 
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import fr.univbrest.dosi.bean.ElementConstitutif;
+import fr.univbrest.dosi.bean.ElementConstitutifPK;
+import fr.univbrest.dosi.bean.Enseignant;
 import fr.univbrest.dosi.bean.Evaluation;
+import fr.univbrest.dosi.bean.Promotion;
+import fr.univbrest.dosi.bean.PromotionPK;
 import fr.univbrest.dosi.repository.EnseignantRepository;
 import fr.univbrest.dosi.repository.EvaluationRepository;
 
@@ -21,7 +28,38 @@ public class EvaluationBusinessJPA implements EvaluationBusiness {
 	@Override
 	public Evaluation AddEvaluation(Evaluation evaluation) {
 
-		return evaluationRepository.save(evaluation);
+		 Enseignant e = new Enseignant();
+			e = evaluation.getEnseignant();
+			int NO_ENSEIGNANT = e.getNoEnseignant();
+			int NO_EVALUATION = evaluation.getNoEvaluation();
+			ElementConstitutif ec = new ElementConstitutif();
+			ElementConstitutifPK ecpk = new ElementConstitutifPK();
+
+			ec = evaluation.getElementConstitutif();
+			ecpk = ec.getId();
+			String CODE_UE = ecpk.getCodeUe();
+			String CODE_EC = ecpk.getCodeEc();
+
+			Promotion p = new Promotion();
+			PromotionPK ppk = new PromotionPK();
+			p = evaluation.getPromotion();
+			ppk = p.getId();
+			String CODE_FORMATION = ppk.getCodeFormation();
+			String ANNEE_UNIVERSITAIRE = ppk.getAnneeUniversitaire();
+
+			String DESIGNATION = evaluation.getDesignation();
+			String ETAT = evaluation.getEtat();
+			String PERIODE = evaluation.getPeriode();
+			Date DEBUT_REPONSE = evaluation.getDebutReponse();
+			Date FIN_REPONSE = evaluation.getFinReponse();
+
+			evaluationRepository.CreateEvaluation(NO_ENSEIGNANT,NO_EVALUATION, CODE_FORMATION, ANNEE_UNIVERSITAIRE,
+					CODE_UE, CODE_EC, DESIGNATION, ETAT, PERIODE, DEBUT_REPONSE, FIN_REPONSE);
+		
+		
+			return evaluation;
+
+		//return evaluationRepository.save(evaluation);
 
 	}
 
--- src/main/java/fr/univbrest/dosi/repository/EvaluationRepository.java
+++ src/main/java/fr/univbrest/dosi/repository/EvaluationRepository.java
@@ -1,9 +1,14 @@
 package fr.univbrest.dosi.repository;
 
+import java.util.Date;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.univbrest.dosi.bean.Evaluation;
 
@@ -13,22 +18,22 @@ public interface EvaluationRepository extends CrudRepository<Evaluation, Integer
 	@Query(value = "SELECT evaluation.* FROM `evaluation` WHERE NO_ENSEIGNANT=?1 ", nativeQuery = true)
 	List <Evaluation> findAllEvaluationByEnseignant(Integer noenseignant);
 
-//	// fonction qui creer une evaluation 
-//		@Transactional
-//		@Modifying
-//		@Query(value = "INSERT INTO `evaluation`(`NO_ENSEIGNANT`, `CODE_FORMATION`, `ANNEE_UNIVERSITAIRE`, `CODE_UE`, `CODE_EC`, `NO_EVALUATION`, `DESIGNATION`, `ETAT`, `PERIODE`, `DEBUT_REPONSE`, `FIN_REPONSE`) VALUES (:NO_ENSEIGNANT, :CODE_FORMATION, :ANNEE_UNIVERSITAIRE, :CODE_UE, :CODE_EC, :NO_EVALUATION, :DESIGNATION, :ETAT, :PERIODE, :DEBUT_REPONSE, :FIN_REPONSE) ", nativeQuery = true)
-//		public void CreateEvaluation(
-//				@Param("NO_ENSEIGNANT") int NO_ENSEIGNANT, 
-//				@Param("NO_EVALUATION") int NO_EVALUATION,
-//				@Param("CODE_FORMATION") String CODE_FORMATION,
-//				@Param("ANNEE_UNIVERSITAIRE") String ANNEE_UNIVERSITAIRE,
-//				@Param("CODE_UE") String CODE_UE,
-//				@Param("CODE_EC") String CODE_EC,
-//				@Param("DESIGNATION") String DESIGNATION,
-//				@Param("ETAT") String ETAT,
-//				@Param("PERIODE") String PERIODE,
-//				@Param("DEBUT_REPONSE") Date DEBUT_REPONSE,
-//				@Param("FIN_REPONSE") Date FIN_REPONSE);
+	// fonction qui creer une evaluation 
+		@Transactional
+		@Modifying
+		@Query(value = "INSERT INTO `evaluation`(`NO_ENSEIGNANT`, `CODE_FORMATION`, `ANNEE_UNIVERSITAIRE`, `CODE_UE`, `CODE_EC`, `NO_EVALUATION`, `DESIGNATION`, `ETAT`, `PERIODE`, `DEBUT_REPONSE`, `FIN_REPONSE`) VALUES (:NO_ENSEIGNANT, :CODE_FORMATION, :ANNEE_UNIVERSITAIRE, :CODE_UE, :CODE_EC, :NO_EVALUATION, :DESIGNATION, :ETAT, :PERIODE, :DEBUT_REPONSE, :FIN_REPONSE) ", nativeQuery = true)
+		public void CreateEvaluation(
+				@Param("NO_EVALUATION") int NO_EVALUATION,
+				@Param("NO_ENSEIGNANT") int NO_ENSEIGNANT, 
+				@Param("CODE_FORMATION") String CODE_FORMATION,
+				@Param("ANNEE_UNIVERSITAIRE") String ANNEE_UNIVERSITAIRE,
+				@Param("CODE_UE") String CODE_UE,
+				@Param("CODE_EC") String CODE_EC,
+				@Param("DESIGNATION") String DESIGNATION,
+				@Param("ETAT") String ETAT,
+				@Param("PERIODE") String PERIODE,
+				@Param("DEBUT_REPONSE") Date DEBUT_REPONSE,
+				@Param("FIN_REPONSE") Date FIN_REPONSE);
 	
 	void deleteByidEvaluation(int idEvaluation);
 	Evaluation findByidEvaluation(Integer idEvaluation);
